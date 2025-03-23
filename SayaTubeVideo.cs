7f3f56153dad75599706dc1d2f3254e8c5bd9f95
Tubagus <tebe.aulia@gmail.com>
2025-03-24 01:53:19 +0700

    menambahkan class SayaTubeVideo

---
+++ SayaTubeVideo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_103022300141
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"Video ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+    }
+}
